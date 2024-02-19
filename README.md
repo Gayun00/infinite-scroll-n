b9733a37c7809e06782765b4fecc35d78073ceb0
= <=>
2024-02-19 17:30:39 +0900

    Docs: update readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 ### 무한 스크롤 구현
+
+- intersectionObserver API와 useInfiniteQuery를 사용해 페이지네이션 구현
+- 로딩 중 Skeleton UI 표시
+- intersectionObserver 롼련 로직을 캡슐화: 동일한 기능을 재사용할 수 있도록 구현
--- src/components/fallbacks/ProductItemSkeleton/index.tsx
+++ src/components/fallbacks/ProductItemSkeleton/index.tsx
@@ -13,9 +13,7 @@ const ProductItemSkeleton = ({ count }: Props) => {
         <Card key={idx} className="pt-5 flex flex-col justify-between">
           <CardContent className="flex flex-col gap-y-5">
             <Skeleton className="w-full aspect-[3/4] rounded-xl" />
-            <div className="space-y-3">
-              <Skeleton className="h-4 w-[250px]" />
-            </div>
+            <Skeleton className="h-4 w-[250px]" />
             <Skeleton className="h-4 w-[200px]" />
           </CardContent>
         </Card>
